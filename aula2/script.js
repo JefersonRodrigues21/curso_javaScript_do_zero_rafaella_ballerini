2b8e982ffdc74adcb7345c4236fa0f4c95bd25a0
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-06-05 17:56:03 -0300

    resolver clicks em branco

--- aula2/script.js
+++ aula2/script.js
@@ -1,9 +1,16 @@
 function adicionarTarefa() {
-        //let mensagem = "Tarefa adicionada com sucesso!";
+        let mensagem = "Tarefa adicionada com sucesso!";
         
-        let inputTarefa = document.getElementById("inputTarefa")
-        let tarefa = inputTarefa.value
-        document.getElementById("mensagem").textContent = tarefa;
+        let inputTarefa = document.getElementById("inputTarefa"); //busca imput tarefa
+        let tarefa = inputTarefa.value; //pega o que é escrito na caixa.
+        document.getElementById("mensagem").textContent = mensagem; //
+
+        let listaTarefas = document.getElementById("listaTarefas"); //pega o elemento ul
+        let novaTarefa = document.createElement("li"); //cria li dentro da variável
+
+        novaTarefa.textContent = tarefa; //conteúdo de texto da tarefa
+
+        listaTarefas.appendChild(novaTarefa); //adciona elemento filho, que é o li
         
-        inputTarefa.value = ""
+        inputTarefa.value = ""; //limpa campo
     }
