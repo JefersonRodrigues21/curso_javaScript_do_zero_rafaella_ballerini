b41da500b57ec1f0a625d4806c5a4b8f1adfdfba
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-06-09 09:19:13 -0300

    continue

--- aula2/script.js
+++ aula2/script.js
@@ -1,16 +1,23 @@
 function adicionarTarefa() {
-        let mensagem = "Tarefa adicionada com sucesso!";
-        
+                
         let inputTarefa = document.getElementById("inputTarefa"); //busca imput tarefa
         let tarefa = inputTarefa.value; //pega o que é escrito na caixa.
-        document.getElementById("mensagem").textContent = mensagem; //
+        
 
-        let listaTarefas = document.getElementById("listaTarefas"); //pega o elemento ul
+        let listaTarefas = document.getElementById("listaTarefas"); //cria novo item (li) e insere na (lista ul)
         let novaTarefa = document.createElement("li"); //cria li dentro da variável
-
         novaTarefa.textContent = tarefa; //conteúdo de texto da tarefa
-
         listaTarefas.appendChild(novaTarefa); //adciona elemento filho, que é o li
         
-        inputTarefa.value = ""; //limpa campo
+        let mensagemSucesso = "Tarefa adicionada com sucesso!";//mensagem de tarefa adicionada com sucesso!
+        document.getElementById("mensagem").textContent = mensagemSucesso; //
+        
+        inputTarefa.value = ""; //limpa o input do usuário
+
+        //se o valor do input for vazio então mostre uma mensagem de erro
+        if (tarefa == "") {
+            //mostre uma mensagem de erro
+            let mensagemErro = "Digite uma tarefa!";
+            document.getElementById("mensagem").textContent = mensagemErro;
+        }
     }
