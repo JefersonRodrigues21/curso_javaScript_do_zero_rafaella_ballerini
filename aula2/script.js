448b89b6775b32ed4c15637423db03bcff08dd8f
JefersonRodrigues21 <dev.geekjs@gmail.com>
2026-07-26 01:12:41 -0300

    Aula 2 finalizada

--- aula2/script.js
+++ aula2/script.js
@@ -1,23 +1,23 @@
 function adicionarTarefa() {
-                
-        let inputTarefa = document.getElementById("inputTarefa"); //busca imput tarefa
-        let tarefa = inputTarefa.value; //pega o que é escrito na caixa.
-        
 
-        let listaTarefas = document.getElementById("listaTarefas"); //cria novo item (li) e insere na (lista ul)
-        let novaTarefa = document.createElement("li"); //cria li dentro da variável
-        novaTarefa.textContent = tarefa; //conteúdo de texto da tarefa
-        listaTarefas.appendChild(novaTarefa); //adciona elemento filho, que é o li
-        
-        let mensagemSucesso = "Tarefa adicionada com sucesso!";//mensagem de tarefa adicionada com sucesso!
-        document.getElementById("mensagem").textContent = mensagemSucesso; //
-        
-        inputTarefa.value = ""; //limpa o input do usuário
+  const inputTarefa = document.getElementById("inputTarefa"); //busca imput tarefa
+  const tarefa = inputTarefa.value.trim(); //pega o valor do input e remove espaços em branco no início e no final (trim - remove espaços em branco)
+  const mensagem = document.getElementById("mensagem"); //busca a div de mensagem
 
-        //se o valor do input for vazio então mostre uma mensagem de erro
-        if (tarefa == "") {
-            //mostre uma mensagem de erro
-            let mensagemErro = "Digite uma tarefa!";
-            document.getElementById("mensagem").textContent = mensagemErro;
-        }
+  //se o valor do input for vazio então mostre uma mensagem de erro
+  if (tarefa == "") {
+    //mostre uma mensagem de erro
+    const mensagemErro = "Digite uma tarefa!";
+    mensagem.textContent = mensagemErro; //mostra a mensagem de erro
+  } else {
+    const mensagemSucesso = "Tarefa adicionada com sucesso!"; //mensagem de tarefa adicionada com sucesso!
+    mensagem.textContent = mensagemSucesso; //mostra a mensagem de sucesso
+
+    const listaTarefas = document.getElementById("listaTarefas"); //cria novo item (li) e insere na (lista ul)
+    const novaTarefa = document.createElement("li"); //cria li dentro da variável
+    novaTarefa.textContent = tarefa; //conteúdo de texto da tarefa
+    listaTarefas.appendChild(novaTarefa); //adciona elemento filho, que é o li
+  }
+
+  inputTarefa.value = ""; //limpa o input do usuário
 }
