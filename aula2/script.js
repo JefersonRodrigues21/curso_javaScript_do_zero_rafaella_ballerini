5a62760e0ef8a9ef0807be232625386c08bbc1eb
JefersonRodrigues21 <dev.geekjs@gmail.com>
2026-07-28 11:46:46 -0300

    Botões editar, remover e limpar tudo inseridos

--- aula2/script.js
+++ aula2/script.js
@@ -41,7 +41,13 @@ function renderizarTarefas() {
     botaoRemover.textContent = "Remover"; //adiciona texto ao botão de remover
     botaoRemover.onclick = () => removerTarefa(i); //adiciona evento de click ao botão de remover/ "=>" para chamar a função removerTarefa
 
+    let botaoEditar = document.createElement('button'); //cria botão de editar
+    botaoEditar.className = 'editar'; //adiciona classe ao botão de editar
+    botaoEditar.textContent = 'Editar'; //adiciona texto ao botão de editar
+    botaoEditar.onclick = () => editarTarefa(i); //adiciona evento de click ao botão de editar
+
     novaTarefa.appendChild(botaoRemover); //adiciona o botão de remover dentro do li
+    novaTarefa.appendChild(botaoEditar); //adiciona o botão de editar dentro do li
     listaTarefas.appendChild(novaTarefa); //adciona elemento filho, que é o li
   }
 }
@@ -49,4 +55,19 @@ function renderizarTarefas() {
 function removerTarefa(i) {
   tarefas.splice(i, 1); //remove a tarefa do array de tarefas
   renderizarTarefas(); //chama a função renderizarTarefas para atualizar a lista de tarefas
+}
+
+function editarTarefa(i) {
+  let tarefaEditada = prompt('Edite a tarefa:');
+  if (tarefaEditada.trim() !== "") {
+    tarefas[i] = tarefaEditada
+    renderizarTarefas(); //chama a função renderizarTarefas para atualizar a lista de tarefas
+  }
+}
+
+function limparLista() {
+  tarefas.length = 0; //limpa a lista de tarefas
+  renderizarTarefas(); //chama a função renderizarTarefas para atualizar a lista de tarefas
+  const mensagem = document.getElementById("mensagem"); //busca a div de mensagem
+  mensagem.textContent = "Lista de tarefas limpa com Sucesso!"; //mostra a mensagem de sucesso
 }
